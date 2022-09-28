71c28dc607a06866b1aa7714a80dcb0578b98678
NahianNahin <nahian1927040@stud.kuet.ac.bd>
2022-09-28 22:11:29 +0600

    install react app and push in github

---
+++ src/components/Activities/Activities.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Activities = () => {
+    return (
+        <div>
+            <div className='activities-container'>
+                <h1>All Activities</h1>
+            </div>
+            <div className='added-area'>
+                <h1>Add your activities time</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Activities;
